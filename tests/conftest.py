1eeda9e1965a086083fd901284d51603280a9b9f
Garvit Bhasin <garvitbhasin@gmail.com>
2026-09-01 20:44:00 +0300

    fix: env accessibility for testing

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,10 +1,22 @@
+import os
+
+# CI pipeline doesn't have access to env variables
+# This temporarily changes db connection from production
+# postgreSQL to SQLite for testing
+TEST_DATABASE_URL = "sqlite:///:memory:"
+os.environ["DB_URL"] = TEST_DATABASE_URL
+
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from backend.database import Base, Users
 
-engine = create_engine("sqlite:///:memory:")
+
+engine = create_engine(
+    TEST_DATABASE_URL,
+    connect_args={"check_same_thread": False},
+)
 
 TestingSessionLocal = sessionmaker(bind=engine)
 
