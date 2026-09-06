c7d6f1c21f30bea7d47541b6af32a78452c9b5ba
Garvit Bhasin <garvitbhasin@gmail.com>
2026-09-06 09:28:57 +0300

    fix: conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,16 +1,11 @@
-import os
-
-# CI pipeline doesn't have access to env variables
-# This temporarily changes db connection from production
-# postgreSQL to SQLite for testing
-TEST_DATABASE_URL = "sqlite:///:memory:"
-os.environ["DB_URL"] = TEST_DATABASE_URL
-
 import pytest
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 from backend.database import Base, Users
+
+
+TEST_DATABASE_URL = "sqlite:///:memory:"
 
 engine = create_engine(
     TEST_DATABASE_URL,
